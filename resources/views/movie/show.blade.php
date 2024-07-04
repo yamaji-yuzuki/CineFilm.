f4817e870de08bf0025b199993ed4409c553e43e
yamaji-yuzuki <22e116019vs@stu.teikyo-u.ac.jp>
2024-07-04 20:52:28 +0900

    APIとコミュニティ#5

--- resources/views/movie/show.blade.php
+++ resources/views/movie/show.blade.php
@@ -8,29 +8,31 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-20">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="grid grid-cols-2 gap-4">
-                        <!-- 左側のカラム -->
-                        <div class="flex flex-col items-center">
-                            <img class="w-1/2" src="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path'] }}" alt="{{ $movie['title'] }}"><br />
-                            <h1 class="mt-4 text-3xl font-bold">{{ $movie['title'] }}</h1><br />
-                            <p class="text-gray-700 text-base mb-4">{{ $movie['overview'] }}</p>
-                        </div>
-                            <!-- 右側のカラム -->
-                            <!-- 予告編を表示 -->
-                            @if(isset($movie['videos']['results']) && count($movie['videos']['results']) > 0)
-                                <div class="mt-4">
-                                    <h2 class="text-xl font-bold mb-2">予告編</h2>
-                                    @foreach($movie['videos']['results'] as $video)
-                                        @if($loop->index < 3) <!-- 最初の3つの予告編を表示 -->
-                                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/{{ $video['key'] }}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="mb-4"></iframe>
-                                        @endif
-                                    @endforeach
+                    <div class="flex flex-wrap">
+                        <!-- 左半分: サムネイルとタイトル -->
+                        <div class="w-full lg:w-1/3 pr-4 mb-4 lg:mb-0">
+                            <div class="rounded overflow-hidden shadow-lg h-full flex flex-col">
+                                <img class="w-full h-96 object-cover" src="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path'] }}" alt="{{ $movie['title'] }}">
+                                <div class="px-6 py-4 flex-1">
+                                    <div class="font-bold text-xl mb-2">{{ $movie['title'] }}</div>
                                 </div>
-                            @else
-                                <p>予告編がありません。</p>
+                            </div>
+                        </div>
+
+                        <!-- 右半分: あらすじと最新の予告編 -->
+                        <div class="w-full lg:w-2/3 pl-4 flex flex-col justify-between">
+                            <div class="flex-1">
+                                <p class="text-gray-700 text-base mb-4">
+                                    {{ $movie['overview'] }}
+                                </p>
+                            </div>
+                            @if ($trailer)
+                                <div class="mt-4">
+                                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/{{ $trailer['key'] }}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>
                             @endif
                         </div>
                     </div>
