b58b131e7d39060bd2a50ca0dba44518b8e69791
Yamaji <167412016+yamaji-yuzuki@users.noreply.github.com>
2024-09-29 13:53:18 +0900

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 <!-- resources/views/dashboard.blade.php -->
 
 <x-app-layout>
+    <title>CineFilm</title>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Now Playing Movies') }}
